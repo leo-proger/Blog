dc0e044caaeb78e2e1ebe47356bff4e5d4e93e91
Leo Proger <leoproger11@gmail.com>
2025-06-30 17:51:48 +0300

    feat: add method to manage likes

--- src/main/java/com/github/leo_proger/blog/services/PostService.java
+++ src/main/java/com/github/leo_proger/blog/services/PostService.java
@@ -1,11 +1,13 @@
 package com.github.leo_proger.blog.services;
 
 import com.github.leo_proger.blog.models.Post;
+import com.github.leo_proger.blog.models.User;
 import com.github.leo_proger.blog.repositories.PostRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -30,5 +32,19 @@ public class PostService {
             throw new IllegalArgumentException("Post with id " + id + " does not exist");
         }
         postRepository.deleteById(id);
+    }
+
+    public void likePost(Long postID, User user) {
+        if (!postRepository.existsById(postID)) {
+            throw new IllegalArgumentException("Post with id " + postID + " does not exist");
+        }
+        Post post = postRepository.getReferenceById(postID);
+        Set<Long> usersLiked = post.getUsersLiked();
+
+        if (usersLiked.contains(user.getId())) {
+            post.removeLike(user.getId());
+        } else {
+            post.addLike(user.getId());
+        }
     }
 }
