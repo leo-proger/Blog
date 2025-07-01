a8cf979835d0e5851f849fdd9a3d67a8614534aa
Leo Proger <leoproger11@gmail.com>
2025-07-01 14:24:39 +0300

    fix: add @GeneratedValue to id field and getter + setters

--- src/main/java/com/github/leo_proger/blog/models/Comment.java
+++ src/main/java/com/github/leo_proger/blog/models/Comment.java
@@ -1,13 +1,18 @@
 package com.github.leo_proger.blog.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Comment {
 
     @Id
+    @GeneratedValue
     private Long id;
 
     @ManyToOne
