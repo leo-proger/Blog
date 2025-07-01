6e9d4ac775230b12337266f0e9a23f075349d260
Leo Proger <leoproger11@gmail.com>
2025-07-01 12:51:35 +0300

    fix: add text field to Comment model

--- src/main/java/com/github/leo_proger/blog/models/Comment.java
+++ src/main/java/com/github/leo_proger/blog/models/Comment.java
@@ -21,6 +21,9 @@ public class Comment {
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
+    @Column(name = "text", nullable = false)
+    private String text;
+
     public Comment() {
         this.createdAt = LocalDateTime.now();
     }
