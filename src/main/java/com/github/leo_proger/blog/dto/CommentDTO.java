b95a828ed388b08b85ae95c96ced4ea91ea742bc
Leo Proger <leoproger11@gmail.com>
2025-07-06 11:37:40 +0300

    feat: add ID field to CommentDTO

--- src/main/java/com/github/leo_proger/blog/dto/CommentDTO.java
+++ src/main/java/com/github/leo_proger/blog/dto/CommentDTO.java
@@ -5,12 +5,14 @@ import com.github.leo_proger.blog.models.Comment;
 import java.time.LocalDateTime;
 
 public record CommentDTO(
+        Long postID,
         String authorName,
         String text,
         LocalDateTime createdAt
 ) {
     public static CommentDTO toDTO(Comment comment) {
         return new CommentDTO(
+                comment.getPost().getId(),
                 comment.getAuthor().getUsername(),
                 comment.getText(),
                 comment.getCreatedAt()
