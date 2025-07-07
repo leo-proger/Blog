31934c5f373a1051807f673105fa2cc271be86ba
Leo Proger <leoproger11@gmail.com>
2025-07-07 17:56:34 +0300

    refactor: convert commentDTO to normal class instead of record

--- src/main/java/com/github/leo_proger/blog/dto/CommentDTO.java
+++ src/main/java/com/github/leo_proger/blog/dto/CommentDTO.java
@@ -1,15 +1,22 @@
 package com.github.leo_proger.blog.dto;
 
 import com.github.leo_proger.blog.models.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-public record CommentDTO(
-        Long postID,
-        String authorName,
-        String text,
-        LocalDateTime createdAt
-) {
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDTO {
+    private Long postID;
+    private String authorName;
+    private String text;
+    private LocalDateTime createdAt;
+
+
     public static CommentDTO toDTO(Comment comment) {
         return new CommentDTO(
                 comment.getPost().getId(),
