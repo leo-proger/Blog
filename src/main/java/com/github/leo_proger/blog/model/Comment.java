22037a05fff75472c6f900126565c080b29e3ee2
Leo Proger <leoproger11@gmail.com>
2025-07-08 16:29:37 +0300

    fix: set some fields unique

--- src/main/java/com/github/leo_proger/blog/model/Comment.java
+++ src/main/java/com/github/leo_proger/blog/model/Comment.java
@@ -16,11 +16,11 @@ public class Comment {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "author_id", nullable = false)
+    @JoinColumn(name = "author_id", nullable = false, unique = true)
     private User author;
 
     @ManyToOne
-    @JoinColumn(name = "post_id", nullable = false)
+    @JoinColumn(name = "post_id", nullable = false, unique = true)
     private Post post;
 
     @Column(nullable = false)
