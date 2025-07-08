9552c19b26bb321548da8e4381031c89277caeea
Leo Proger <leoproger11@gmail.com>
2025-07-08 16:48:44 +0300

    refactor: remove setters for PostDTO

--- src/main/java/com/github/leo_proger/blog/dto/PostDTO.java
+++ src/main/java/com/github/leo_proger/blog/dto/PostDTO.java
@@ -3,17 +3,14 @@ package com.github.leo_proger.blog.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostDTO {
     private String text;
-
 
     private MultipartFile imageFile;
 
