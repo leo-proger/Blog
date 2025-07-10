aeb66dd06beee94689a6c29fecee5cfa1c5a94fa
Leo Proger <leoproger11@gmail.com>
2025-07-10 11:11:51 +0300

    fix: inapplicable annotation

--- src/main/java/com/github/leo_proger/blog/dto/CommentDTO.java
+++ src/main/java/com/github/leo_proger/blog/dto/CommentDTO.java
@@ -1,12 +1,22 @@
 package com.github.leo_proger.blog.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class CommentDTO {
+    @NotEmpty(message = "Author can't be empty")
+    @NotBlank(message = "Author can't be empty")
     private final String authorName;
+
+    @NotNull(message = "Post ID can't be empty")
     private final Long postID;
+
+    @NotEmpty(message = "Text can't be empty")
+    @NotBlank(message = "Text can't be empty")
     private final String text;
 }
